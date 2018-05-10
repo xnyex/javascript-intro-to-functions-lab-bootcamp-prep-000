ddc2649b161a30c1149e833e733b1488215f1ce7
Nye Smith <nyedess@gmail.com>
2018-05-10 00:14:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
   if string === string.toLowerCase() {
     return "I can't hear you!"
   } else if string === string.toUpperCase {
-    return ""
-  }
+    return "YES INDEED"
+  } else if string ===
 }
 
