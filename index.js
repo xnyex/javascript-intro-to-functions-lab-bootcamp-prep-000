443a1a7c9772cb13f89577adac6b735cad23db2f
Nye Smith <nyedess@gmail.com>
2018-05-10 00:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ function logWhisper(string){
   console.log(string)
 }
 function sayHiToGrandma(string){
-  if string = string.toLowerCase() {
+  if (string="hello"){
     return "I can't hear you!"
-  } else if string = string.toUpperCase() {
-    return "YES INDEED"
-  } else if string = "I love you, grandma" {
-    return "I love you, too."
+  } else if (string="HELLO"){
+    return "YES INDEED!"
   }
 }
 
