3fa2ad584b0699dffb7aff1108941d66055e23b6
Nye Smith <nyedess@gmail.com>
2018-05-10 00:06:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string){
+  string = string.toUpperCase();
+  console.log (string)
+}
+function logWhisper(string){
+  string = string.toLowerCase();
+  console.log(string)
+}
+
+
